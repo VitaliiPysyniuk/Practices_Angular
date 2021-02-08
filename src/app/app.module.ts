a5b9d1c4149ae130a70fd8dffaded9e3dd697675
Vitalii <Viitaliy2112@gmail.com>
2021-02-08 21:29:13 +0200

    practice_#6_done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import { MainComponent } from './components/main/main.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { PostComponent } from './components/post/post.component';
+import {HttpClientModule} from "@angular/common/http";
+import { SelectedPostComponent } from './components/selected-post/selected-post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    PostsComponent,
+    PostComponent,
+    SelectedPostComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
